c448245e9bfb4aa7b4688b62b7abb1330bdd038c
mj <mj577900@gmail.com>
2022-05-30 16:09:32 +0900

    add:logout

--- server/controllers/users/logout.js
+++ server/controllers/users/logout.js
@@ -1,5 +1,12 @@
 /** 로그아웃 요청 */
 
 module.exports = (req,res) => {
-
+    if(req.cookies) {
+        res.cookie('token', null, {
+            maxAge: 0
+        })
+        res.json({message: '로그아웃 성공'})
+    } else {
+        res.json({message: '서버에 문제가 있네요'})
+    }
 }
