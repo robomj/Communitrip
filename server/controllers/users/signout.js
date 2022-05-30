b100a87fbc2f535d0bc54a11b93403bfb10828ac
mj <mj577900@gmail.com>
2022-05-30 16:46:15 +0900

    add:signout

--- server/controllers/users/signout.js
+++ server/controllers/users/signout.js
@@ -1,5 +1,19 @@
 /** 회원탈퇴 요청 */
+const { users } = require('../../models')
 
 module.exports = (req,res) => {
-
+    users.destroy({
+        where: {
+            id: req.params.userId
+        }
+    }).then((result) => {
+        if(!result) {
+            res.status(404).json({ message: '존재하지 않는 유저입니다' })
+        } else {
+            res.json({ message: '회원탈퇴 성공'})
+        }
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).json({ message: '서버에 문제가 있네요'})
+    })
 }
