35bc28bd456f92fda815324d1b8c124563c5c0c6
mj <mj577900@gmail.com>
2022-06-11 22:41:58 +0900

    change:mypost

--- server/controllers/posts/mypost.js
+++ server/controllers/posts/mypost.js
@@ -2,7 +2,10 @@
 const { posts, likes } = require('../../models')
 
 module.exports = (req, res) => {
-    likes.findOne({ /** params로 받은 userId에 해당하는 likes 모두 찾아라 */
+    likes.findOne({
+        attributes: {
+            exclude: ['postId', 'userId']
+        }, /** params로 받은 userId에 해당하는 likes 모두 찾아라 */
         where: {
             user_id: req.params.userId
         }
@@ -11,6 +14,9 @@ module.exports = (req, res) => {
             res.status(404).json({ message: '좋아요 누른 게시글이 없어요' })
         } else {
             posts.findAll({
+                attributes: {
+                    exclude: ['postId', 'userId']
+                },
                 where: {
                     id: result.dataValues.post_id
                 }
