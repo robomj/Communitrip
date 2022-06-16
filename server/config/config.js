0210ca746c1ac0281f1377918826ed4feb6979b4
JANGto1 <dadadaca12@gmail.com>
2022-06-16 22:08:44 +0900

    fix: .env 수정

--- server/config/config.js
+++ server/config/config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports =
 {
   development: {
-    username: 'root',
-    password: 'password',
-    database: 'final_project',
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     host: '127.0.0.1',
     dialect: 'mysql'
   },
